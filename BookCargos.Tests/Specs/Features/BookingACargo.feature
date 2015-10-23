b69d49c0b4f6996fecd377737ae78b9016a3ff2c
Parajao <alex.digioia@gmail.com>
2015-10-23 18:48:53 +0100

    Added features and steps definitions

---
+++ BookCargos.Tests/Specs/Features/BookingACargo.feature
@@ -0,0 +1,16 @@
+﻿Feature: BookingACargo
+	In order to send a cargo on a route
+	As a cargo owner
+	I want to book a cargo on a vessel
+
+Scenario: booking a cargo on a vessel with enough capacity left
+	Given a vessel with enough capacity left
+	When I book a cargo
+	Then the cargo is added to the vessel
+	And I receive a booking confirmation
+
+Scenario: booking a cargo on a vessel without enough capacity left
+	Given a full vessel without enough capacity left
+	When I book a cargo
+	Then the cargo is not added to the vessel
+	And I receive a notification that the vessel has not capacity
---
+++ BookCargos.Tests/Specs/Steps/BookingACargoSteps.cs
@@ -0,0 +1,50 @@
+﻿using TechTalk.SpecFlow;
+
+namespace BookCargos.Tests.Specs.Steps
+{
+    [Binding]
+    public class BookingACargoSteps
+    {
+        [Given(@"a vessel with enough capacity left")]
+        public void GivenAVesselWithEnoughCapacityLeft()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"a full vessel without enough capacity left")]
+        public void GivenAFullVesselWithoutEnoughCapacityLeft()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I book a cargo")]
+        public void WhenIBookACargo()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the cargo is added to the vessel")]
+        public void ThenTheCargoIsAddedToTheVessel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I receive a booking confirmation")]
+        public void ThenIReceiveABookingConfirmation()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the cargo is not added to the vessel")]
+        public void ThenTheCargoIsNotAddedToTheVessel()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I receive a notification that the vessel has not capacity")]
+        public void ThenIReceiveANotificationThatTheVesselHasNotCapacity()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
